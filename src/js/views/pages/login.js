a97f46dfd44d1749646a30792917fb2ffec647fe
Kushagra <kkss420@gmail.com>
2018-02-25 17:47:25 +0530

    opening register page

--- src/js/views/pages/login.js
+++ src/js/views/pages/login.js
@@ -1,5 +1,3 @@
-$.getScript('globals.js');
-
 $(function () {
     'use strict';
     if (localStorage.getItem('auth_token')) {
@@ -11,12 +9,16 @@ $(function () {
         login($('input:first').val(), $('input:last').val(), function (data) {
             localStorage.setItem('auth_token', data.token);
             localStorage.setItem('user', JSON.stringify(data.user));
-            location.reload();
+            window.location = '../../index.html';
         }, function (error) {
             $('.container')
                 .prepend(`<div class="alert alert-danger alert-dismissible slideIn show" role="alert">${error.responseJSON.message}<button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button></div>`);
         });
+    });
+
+    $(document).on('click', '#signUpButton', function () {
+       window.location.href = 'register.html';
     });
 });
